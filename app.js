57c013a385e08ea01ff97a651c73b79de0b09937
Daud Ahmad <daudahmedkhokhar@gmail.com>
2018-06-27 23:21:59 +1000

    Added logic to pick ip address

--- app.js
+++ app.js
@@ -5,14 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ipStackAccessKey = "7753621da6f9525de5242d736aa995ee";
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get("/", function(req, res) {
-  res.send("hello world");
-});
-
 app.get("/api/formatPhoneNumber", function(req, res) {
-  console.log(req.query);
-  console.log(req.ip);
+  //   console.log(req.query);
+  //   console.log(req.ip);
   let number = req.query.number;
   let countryCode = req.query.country_code;
   //   let ip = req.query.ip;
@@ -31,32 +26,42 @@ app.get("/api/formatPhoneNumber", function(req, res) {
       `http://api.ipstack.com/${ip}?access_key=${ipStackAccessKey}&format=1`,
       function(error, response, body) {
         if (!error && response.statusCode == 200) {
-          const info = JSON.parse(body);
-          countryCode = info.country_code;
-          console.log(`Country code looked up: ${countryCode}`);
-          if (countryCode === undefined) {
-            res.json({ error: "country code could not be determined" });
-          } else {
-            try {
-              formattedNumber = libphonenumber.formatNumber(
-                number,
-                countryCode,
-                "International"
-              );
-              let valid = libphonenumber.isValidNumber(formattedNumber, countryCode);
-              res.json({ formatted_number: formattedNumber, valid: valid });
-            } catch (error) {
-              console.error(error);
-              res.json({ error: "invalid number" });
+          try {
+            const info = JSON.parse(body);
+            countryCode = info.country_code;
+            //   console.log(`Country code looked up: ${countryCode}`);
+            if (countryCode === undefined) {
+              res.json({ error: "country code could not be determined" });
+            } else {
+              formatNumber(number, countryCode, res);
             }
+          } catch (error) {
+            res.json({ error: "invalid number" });
           }
         } else {
           res.json({ error: "country code could not be determined" });
         }
       }
     );
+  } else {
+    formatNumber(number, countryCode, res);
   }
 });
+
+function formatNumber(number, countryCode, res) {
+  try {
+    formattedNumber = libphonenumber.formatNumber(
+      number,
+      countryCode,
+      "International"
+    );
+    let valid = libphonenumber.isValidNumber(formattedNumber, countryCode);
+    res.json({ formatted_number: formattedNumber, valid: valid });
+  } catch (error) {
+    console.error(error);
+    res.json({ error: "invalid number" });
+  }
+}
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
