30bc8ff7ed9c79e2be4cad65e8d2d148c8f3a96c
Daud Ahmad <daudahmedkhokhar@gmail.com>
2018-06-28 21:06:33 +1000

    Deleted some code

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ const jwksRsa = require("jwks-rsa");
 const constants = require("./constants");
 const PORT = process.env.PORT || 3000;
 
-// Authentication middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
 const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
@@ -31,7 +25,9 @@ const checkJwt = jwt({
 app.get("/api/formatPhoneNumber", checkJwt, function(req, res) {
   let number = req.query.number;
   let countryCode = req.query.country_code;
-  //   let ip = req.query.ip;
+  // let ipAddress = req.query.ip;
+  // let ipAddress = req.connection.remoteAddress;
+  // console.log(req.connection);
   let formattedNumber = "";
 
   if (number === undefined) {
@@ -43,29 +39,33 @@ app.get("/api/formatPhoneNumber", checkJwt, function(req, res) {
       const ip = req.headers["x-forwarded-for"];
       // const ip = "203.214.65.84";
       console.log(`x-forwarded-for: ${ip}`);
-      request(
-        `http://api.ipstack.com/${ip}?access_key=${
-          constants.ipStackAccessKey
-        }&format=1`,
-        function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            try {
-              // const info = JSON.parse(body);
-              countryCode = JSON.parse(body).country_code;
-              //   console.log(`Country code looked up: ${countryCode}`);
-              if (countryCode === undefined) {
-                sendErrorMessage(res, constants.countryCodeNotDeterminedMsg);
-              } else {
-                formatNumber(number, countryCode, res);
+      if (ip === undefined) {
+        sendErrorMessage(res, constants.countryCodeNotDeterminedMsg);
+      } else {
+        request(
+          `http://api.ipstack.com/${ip}?access_key=${
+            constants.ipStackAccessKey
+          }&format=1`,
+          function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              try {
+                // const info = JSON.parse(body);
+                countryCode = JSON.parse(body).country_code;
+                //   console.log(`Country code looked up: ${countryCode}`);
+                if (countryCode === undefined) {
+                  sendErrorMessage(res, constants.countryCodeNotDeterminedMsg);
+                } else {
+                  formatNumber(number, countryCode, res);
+                }
+              } catch (error) {
+                sendErrorMessage(res, constants.invalidNumberMsg);
               }
-            } catch (error) {
-              sendErrorMessage(res, constants.invalidNumberMsg);
+            } else {
+              sendErrorMessage(res, constants.countryCodeNotDeterminedMsg);
             }
-          } else {
-            sendErrorMessage(res, constants.countryCodeNotDeterminedMsg);
           }
-        }
-      );
+        );
+      }
     } else {
       formatNumber(number, countryCode, res);
     }
@@ -101,7 +101,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.use(function(req, res) {
-  res.send(404);
+  res.status(404);
 });
 
 app.listen(PORT, () =>
